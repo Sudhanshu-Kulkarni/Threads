3d84b4890b9f8e55a3ed3f9718641b48ba84dfcb
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-04-20 23:20:43 +0530

    Add run method to invoke printNumbers

--- PrintNumbers.java
+++ PrintNumbers.java
@@ -1,5 +1,8 @@
 // This class implements Runnable to print numbers from 1 to 5
 
 public class PrintNumbers implements Runnable {
-    
+    @Override
+    public void run() {
+        printNumbers();
+    }
 }
