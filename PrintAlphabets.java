75896d0fdfd5c3c05f7331f1aa3198ac6df3a838
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-04-20 23:19:43 +0530

    Implement printAlphabets method to print A to E with 500ms delay

--- PrintAlphabets.java
+++ PrintAlphabets.java
@@ -7,7 +7,14 @@ public class PrintAlphabets implements Runnable {
     }
     // Function to print alphabets from A to E
     public void printAlphabets() {
-        
+        for (char ch = 'A'; ch <= 'E'; ch++) {
+            System.out.println("Alphabet: " + ch);
+            try {
+                Thread.sleep(500); // delay for visibility
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+            }
+        }
     }
 
 }
