0e70b3e696ab1d6ccd7c72c0ade6f0c101ab5dca
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-04-20 23:21:57 +0530

    Create Main class and define threads for numbers and alphabets

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Name: Sudhanshu Kulkarni
+// PRN: 23070126132
+// Batch: B3
+
+// This is the main file that initiates all threads.
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
